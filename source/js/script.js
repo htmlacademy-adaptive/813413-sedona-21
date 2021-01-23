5f3f744aaf2ffef4f8d9782828a5ed7e76aa1166
YuriiTryhub <yuriitrihub@gmail.com>
2021-01-23 15:10:35 +0200

    Мобильное меню работоспособное при отключённом JavaScript (Б30)

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,14 @@ const buttonMenu = document.querySelector(".main-nav__toggle");
 const buttonCloseMenu = document.querySelector(".main-nav__close-button");
 const menuNavigation = document.querySelector(".main-nav__list");
 
+document.addEventListener('DOMContentLoaded', function(){
+  menuNavigation.classList.add("main-nav__hide");
+  buttonCloseMenu.classList.add("main-nav__visual");
+  buttonMenu.classList.add("main-nav__visual");
+  buttonCloseMenu.classList.remove("main-nav__hide");
+  buttonMenu.classList.remove("main-nav__hide");
+});
+
 buttonMenu.addEventListener("click", function (evt) {
   evt.preventDefault();
   menuNavigation.classList.add("main-nav__visual");
